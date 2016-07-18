1fd32dba923bcba0e15c22c602d71cd75080c763
Zhiyan Xie <xiezhiyanhfut@hotmail.com>
2016-07-18 16:50:28 -0500

    add parent directory features

--- app/index.js
+++ app/index.js
@@ -1,10 +1,19 @@
 'use strict';
 var generators = require('yeoman-generator'),
-    _ = require('lodash');
+    _ = require('lodash'),
+    chalk = require('chalk'),
+    yosay = require('yosay');
 
 module.exports = generators.NamedBase.extend({
+    end: function(){
+      this.log(chalk.green.bold('Installation successful!'));
+      this.log(chalk.yellow.bold('Remember add dependencies to parent or current module, directive!'));
+      //
+    },
+
     constructor: function () {
         generators.NamedBase.apply(this, arguments);
+        // console.log(arguments);
 
         this.option('view', {
             desc: 'Determines if view is created along with controller',
@@ -14,11 +23,18 @@ module.exports = generators.NamedBase.extend({
     },
 
     writing: function(){
+        console.log(this. _args[1]);// get the second parameter as the path
+        if (this._args.length>1){
+          var parent = getFileNameFragment(this._args[1]) + '/';
+        } else {
+          var parent = '';
+        }
+
         var fileNameFragment = getFileNameFragment(this.name);
 
         this.fs.copyTpl(
             this.templatePath('_module.js'),
-            this.destinationPath('src/components/' + fileNameFragment + '/' + fileNameFragment + '-module.js'),
+            this.destinationPath('src/components/'+ parent + fileNameFragment + '/' + fileNameFragment + '-module.js'),
             {
                 componentName: this.name,
             }
@@ -26,7 +42,7 @@ module.exports = generators.NamedBase.extend({
 
         this.fs.copyTpl(
             this.templatePath('_controller.js'),
-            this.destinationPath('src/components/' + fileNameFragment + '/' + fileNameFragment + '-controller.js'),
+            this.destinationPath('src/components/' + parent + fileNameFragment + '/' + fileNameFragment + '-controller.js'),
             {
                 componentName: this.name,
             }
@@ -34,7 +50,7 @@ module.exports = generators.NamedBase.extend({
 
         this.fs.copyTpl(
             this.templatePath('_service.js'),
-            this.destinationPath('src/components/' + fileNameFragment + '/' + fileNameFragment + '-service.js'),
+            this.destinationPath('src/components/' + parent + fileNameFragment + '/' + fileNameFragment + '-service.js'),
             {
                 componentName: this.name,
             }
@@ -42,7 +58,7 @@ module.exports = generators.NamedBase.extend({
 
         this.fs.copyTpl(
             this.templatePath('_directive.js'),
-            this.destinationPath('src/components/' + fileNameFragment + '/' + fileNameFragment + '-directive.js'),
+            this.destinationPath('src/components/' + parent + fileNameFragment + '/' + fileNameFragment + '-directive.js'),
             {
                 componentName: this.name,
             }
@@ -50,7 +66,7 @@ module.exports = generators.NamedBase.extend({
         if (this.options.view) {
             this.fs.copyTpl(
                 this.templatePath('_index.html'),
-                this.destinationPath('src/components/' + fileNameFragment + '/' + fileNameFragment + '.html'),
+                this.destinationPath('src/components/' + parent + fileNameFragment + '/' + fileNameFragment + '.html'),
                 {
                     componentName: this.name
                 });
@@ -59,5 +75,9 @@ module.exports = generators.NamedBase.extend({
         function getFileNameFragment(componentName) {
             return _.kebabCase(componentName);
         }
+
+
     }
+
+
 });
