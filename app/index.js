ea75abdf9d72259344eb8109cfa9aff150ef9df5
Zhiyan Xie <xiezhiyanhfut@hotmail.com>
2016-07-18 15:19:53 -0500

    create different templates and edit index.js

---
+++ app/index.js
@@ -0,0 +1,64 @@
+'use strict';
+var generators = require('yeoman-generator'),
+    _ = require('lodash');
+
+module.exports = generators.NamedBase.extend({
+    constructor: function () {
+        generators.NamedBase.apply(this, arguments);
+
+        this.option('view', {
+            desc: 'Determines if view is created along with controller',
+            type: Boolean,
+            default: false
+        });
+    },
+
+    writing: function(){
+        var fileNameFragment = getFileNameFragment(this.name);
+
+        this.fs.copyTpl(
+            this.templatePath('_module.js'),
+            this.destinationPath('src/components/' + fileNameFragment + '/' + fileNameFragment + '-module.js'),
+            {
+                componentName: this.name,
+            }
+        );
+
+        this.fs.copyTpl(
+            this.templatePath('_controller.js'),
+            this.destinationPath('src/components/' + fileNameFragment + '/' + fileNameFragment + '-controller.js'),
+            {
+                componentName: this.name,
+            }
+        );
+
+        this.fs.copyTpl(
+            this.templatePath('_directive.js'),
+            this.destinationPath('src/components/' + fileNameFragment + '/' + fileNameFragment + '-directive.js'),
+            {
+                componentName: this.name,
+            }
+        );
+
+        this.fs.copyTpl(
+            this.templatePath('_service.js'),
+            this.destinationPath('src/components/' + fileNameFragment + '/' + fileNameFragment + '-service.js'),
+            {
+                componentName: this.name,
+            }
+        );
+
+        if (this.options.view) {
+            this.fs.copyTpl(
+                this.templatePath('_index.html'),
+                this.destinationPath('src/components/' + fileNameFragment + '/' + fileNameFragment + '.html'),
+                {
+                    componentName: this.name
+                });
+        }
+
+        function getFileNameFragment(componentName) {
+            return _.kebabCase(componentName);
+        }
+    }
+});
