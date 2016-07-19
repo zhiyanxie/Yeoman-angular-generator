beaabef17338e50a4937f7a6c62907b8f67b6ae4
Zhiyan Xie <xiezhiyanhfut@hotmail.com>
2016-07-19 10:31:16 -0500

    add selection

--- app/index.js
+++ app/index.js
@@ -11,15 +11,62 @@ module.exports = generators.NamedBase.extend({
       //
     },
 
+    prompting: function(){
+            var done = this.async();
+            this.prompt([
+            {
+                type: 'checkbox',
+                name: 'jslibs',
+                message: 'Which templates would you want to include for your component?',
+                choices: [
+                    {
+                        name: 'module.js',
+                        value: 'module.js',
+                        checked: true
+                    },
+                    {
+                        name: 'controller.js',
+                        value: 'controller.js',
+                        checked: true
+                    },
+                    {
+                        name: 'service.js',
+                        value: 'service.js',
+                        checked: true
+                    },
+                    {
+                        name: 'directive.js',
+                        value: 'directive.js',
+                        checked: true
+                    },
+                    {
+                        name: 'test.js',
+                        value: 'test.js',
+                        checked: true
+                    },
+                    {
+                        name: 'directive.html',
+                        value: 'directive.html',
+                        checked: true
+                    }
+                ]
+            }], function(answers){
+               this.log(answers);
+               //this.ngappname = answers.ngappname;
+               this.includeModule = _.includes(answers.jslibs, 'module.js');
+               this.includeController = _.includes(answers.jslibs, 'controller.js');
+               this.includeService = _.includes(answers.jslibs, 'service.js');
+               this.includeDirective = _.includes(answers.jslibs, 'directive.js');
+               this.includeTest = _.includes(answers.jslibs, 'test.js');
+               this.includeView = _.includes(answers.jslibs, 'directive.html');
+               done();
+            }.bind(this));
+
+    },
+
     constructor: function () {
         generators.NamedBase.apply(this, arguments);
         // console.log(arguments);
-
-        this.option('view', {
-            desc: 'Determines if view is created along with controller',
-            type: Boolean,
-            default: false
-        });
     },
 
     writing: function(){
@@ -33,53 +80,67 @@ module.exports = generators.NamedBase.extend({
         } else {
           var parent = '';
         }
-        console.log(parent);
+        
         var fileNameFragment = getFileNameFragment(this.name);
 
-        this.fs.copyTpl(
-            this.templatePath('_module.js'),
-            this.destinationPath('src/components/'+ parent + fileNameFragment + '/' + fileNameFragment + '-module.js'),
-            {
-                componentName: this.name,
-            }
-        );
+        if (this.includeModule){
+          this.fs.copyTpl(
+              this.templatePath('_module.js'),
+              this.destinationPath('src/components/'+ parent + fileNameFragment + '/' + fileNameFragment + '-module.js'),
+              {
+                  componentName: this.name,
+              }
+          );
+        };
 
-        this.fs.copyTpl(
-            this.templatePath('_controller.js'),
-            this.destinationPath('src/components/' + parent + fileNameFragment + '/' + fileNameFragment + '-controller.js'),
-            {
-                componentName: this.name,
-            }
-        );
 
-        this.fs.copyTpl(
-            this.templatePath('_service.js'),
-            this.destinationPath('src/components/' + parent + fileNameFragment + '/' + fileNameFragment + '-service.js'),
-            {
-                componentName: this.name,
-            }
-        );
+        if (this.includeController){
+          this.fs.copyTpl(
+              this.templatePath('_controller.js'),
+              this.destinationPath('src/components/' + parent + fileNameFragment + '/' + fileNameFragment + '-controller.js'),
+              {
+                  componentName: this.name,
+              }
+          );
+        };
 
-        this.fs.copyTpl(
-            this.templatePath('_directive.js'),
-            this.destinationPath('src/components/' + parent + fileNameFragment + '/' + fileNameFragment + '-directive.js'),
-            {
-                componentName: this.name,
-            }
-        );
+        if (this.includeService){
+          this.fs.copyTpl(
+              this.templatePath('_service.js'),
+              this.destinationPath('src/components/' + parent + fileNameFragment + '/' + fileNameFragment + '-service.js'),
+              {
+                  componentName: this.name,
+              }
+          );
+        };
 
-        this.fs.copyTpl(
-            this.templatePath('_test.js'),
-            this.destinationPath('src/components/' + parent + fileNameFragment + '/' + fileNameFragment + '-test.js'),
-            {
-                componentName: this.name,
-                componentDashName: fileNameFragment
-            }
-        );
 
-        if (this.options.view) {
+        if (this.includeDirective){
+          this.fs.copyTpl(
+              this.templatePath('_directive.js'),
+              this.destinationPath('src/components/' + parent + fileNameFragment + '/' + fileNameFragment + '-directive.js'),
+              {
+                  componentName: this.name,
+              }
+          );
+        };
+
+
+        if (this.includeTest){
+          this.fs.copyTpl(
+              this.templatePath('_test.js'),
+              this.destinationPath('src/components/' + parent + fileNameFragment + '/' + fileNameFragment + '-test.js'),
+              {
+                  componentName: this.name,
+                  componentDashName: fileNameFragment
+              }
+          );
+        };
+
+
+        if (this.includeView) {
             this.fs.copyTpl(
-                this.templatePath('_index.html'),
+                this.templatePath('_directive.html'),
                 this.destinationPath('src/components/' + parent + fileNameFragment + '/' + fileNameFragment + '.html'),
                 {
                     componentName: this.name
